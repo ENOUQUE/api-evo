ce1474d3c3c28a44e4ee6e30747a3b3ffe62d82b
ENOUQUE <125835180+ENOUQUE@users.noreply.github.com>
2026-01-18 01:32:28 -0300

    aps

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Evolution Manager</h1>
+      <p>Application is running...</p>
+    </div>
+  )
+}
+
+export default App
